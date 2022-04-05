235ec7b4c6b06c0d26cab2691c4c6da6ab676db3
ayaskanti <ayaskanti.das.26@gmail.com>
2022-04-05 22:49:42 +0530

    first commit

---
+++ static.js
@@ -0,0 +1,273 @@
+var team_data = [
+
+    {
+        "team_no": 1,
+        "teamFullName": "Royal Challenges Banglore",
+        "teamSortName": "RCB",
+        "teamLogo": "RCB.png.jpg",
+        "topBatsMan": "virat Kohil",
+        "topBolower": "Aaron Finch",
+        "totalPlayer": 20,
+        "wonMatch": 5,
+        "player": [
+            {
+                "name": "Virat Kohil",
+                "photo": "virat.jpg.webp",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            // {
+            //     "name": "AB de Villiaus",
+            //     "photo": "ab de villiers.jpg.jpg",
+            //     "team": "RCB",
+            //     "price": "20cr",
+            //     "status": "Playing",
+            //     "Role": "Batsman"
+            // },
+            {
+                "name": "Yuzvendra Chahal",
+                "photo": "Yuzvendra.jpg.jpeg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "Aaron Finch",
+                "photo": "aron.jpg.jpeg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Bolower"
+            }
+        ]
+    },
+    {
+        "team_no": 2,
+        "teamFullName": "Mumbai Indians",
+        "teamSortName": "MI",
+        "teamLogo": "mi.png.webp",
+        "topBatsMan": "Rohit sarma",
+        "topBolower": "Hardik pandia",
+        "totalPlayer": 15,
+        "wonMatch": 10,
+        "player": [
+            {
+                "name": "Rohit Sharma",
+                "photo": "rohit.jpg.jpg",
+                "team": "MI",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "Hardik Pandya",
+                "photo": "Hardik.jpg.jpeg",
+                "team": "MI",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Bolower"
+            },
+            // {
+            //     "name": "Daniel Sams",
+            //     "photo": "Daniel.jpg.jpeg",
+            //     "team": "MI",
+            //     "price": "20cr",
+            //     "status": "Playing",
+            //     "Role": "Batsman"
+            // },
+            {
+                "name": "Riley Meredith",
+                "photo": "Riley.jpg.jpeg",
+                "team": "MI",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            }
+        ]
+    },
+    {
+        "team_no": 3,
+        "teamFullName": "Chennai Super King",
+        "teamSortName": "CSK",
+        "teamLogo": "csk.jpg",
+        "topBatsMan": "MS Dhoni",
+        "topBolower": "Adam Milne",
+        "totalPlayer": 15,
+        "wonMatch": 10,
+        "player": [
+            {
+                "name": "MS Dhoni",
+                "photo": "dhoni.jpg.webp",
+                "team": "CSK",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "Deepak Chahar",
+                "photo": "Deepak.jpg.jpeg",
+                "team": "CSK",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "Adam Milne",
+                "photo": "Adam.jpg.jpeg",
+                "team": "CSK",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Bolower"
+            },
+            // {
+            //     "name": "Moeen Ali",
+            //     "photo": "Moeen.jpg.jpeg",
+            //     "team": "CSK",
+            //     "price": "20cr",
+            //     "status": "Playing",
+            //     "Role": "Batsman"
+            // }
+        ]
+
+    },
+    {
+        "team_no": 4,
+        "teamFullName": "Panjab",
+        "teamSortName": "PNB",
+        "teamLogo": "pnb.jpg",
+        "topBatsMan": "KL Rahul",
+        "topBolower": "Shikhar Dhawan",
+        "totalPlayer": 15,
+        "wonMatch": 10,
+        "player": [
+            {
+                "name": "KL Rahul",
+                "photo": "rahul.jpg.jpg",
+                "team": "PNB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "Shikhar Dhawan",
+                "photo": "shikhar.jpg.jpeg",
+                "team": "PNB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Bolower"
+            },
+            // {
+            //     "name": "Benny Howell",
+            //     "photo": "Benny.jpg.jpg",
+            //     "team": "PNB",
+            //     "price": "20cr",
+            //     "status": "Playing",
+            //     "Role": "Batsman"
+            // },
+            {
+                "name": "Odean Smith",
+                "photo": "Odean.jpg.jpeg",
+                "team": "PNB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            }
+        ]
+
+    },
+    {
+        "team_no": 5,
+        "teamFullName": "Kolkata Knight Riders",
+        "teamSortName": "KKR",
+        "teamLogo": "kkr.png.jpg",
+        "topBatsMan": "Shreyas Lyer",
+        "topBolower": "Sam Billings",
+        "totalPlayer": 15,
+        "wonMatch": 10,
+        "player": [
+            {
+                "name": "Shreyas Lyer",
+                "photo": "shreyas.jpg.jpg",
+                "team": "KKR",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            {
+                "name": "Mohammad Nabi",
+                "photo": "Mohammad.jpg.jpeg",
+                "team": "KKR",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            // {
+            //     "name": "Sam Billings",
+            //     "photo": "Sam.jpg.jpeg",
+            //     "team": "KKR",
+            //     "price": "20cr",
+            //     "status": "Playing",
+            //     "Role": "Bolower"
+            // },
+            {
+                "name": "Nitish Rana",
+                "photo": "Nitish.jpg.jpeg",
+                "team": "KKR",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            }
+        ]
+
+    },
+    {
+        "team_no": 6,
+        "teamFullName": "Delhi Capitals",
+        "teamSortName": "DC",
+        "teamLogo": "dc.png.jpg",
+        "topBatsMan": "Rishabh Pant",
+        "topBolower": "Kamlesh Nagarkoti",
+        "totalPlayer": 25,
+        "wonMatch": 10,
+        "player": [
+            {
+                "name": "Rishabh Pant",
+                "photo": "rishabh.jpg.jpg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            },
+            // {
+            //     "name": "Tim Seifert",
+            //     "photo": "Tim.jpg.jpeg",
+            //     "team": "RCB",
+            //     "price": "20cr",
+            //     "status": "Playing",
+            //     "Role": "Batsman"
+            // },
+            {
+                "name": "Kamlesh Nagarkoti",
+                "photo": "Kamlesh.jpg.jpeg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Bolower"
+            },
+            {
+                "name": "Mitchell Marsh",
+                "photo": "Mitchell.jpg.jpeg",
+                "team": "RCB",
+                "price": "20cr",
+                "status": "Playing",
+                "Role": "Batsman"
+            }
+        ]
+
+    }
+]
+
+localStorage.setItem('team_data', JSON.stringify(team_data))
